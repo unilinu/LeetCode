79a63394baca47b4d860bd85896074a4ece80b43
unilinu <unilinu@qq.com>
2024-07-07 23:14:20 +0800

    code101 day95

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	fmt.Println(wordBreak("abc", []string{"a", "bb", "c"}))
+
+}
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for _, word := range wordDict {
+			if strings.HasSuffix(s[:i], word) {
+				dp[i] = dp[i] || dp[i-len(word)]
+			}
+		}
+	}
+
+	return dp[len(s)]
 }
